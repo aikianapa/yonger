5388e78378956517cc4e88cc0f3293e707d66754
Blacker <oleg_frolov@mail.ru>
2021-01-28 11:35:21 +0300

    underconstruction

---
+++ tpl/undefconstruction.php
@@ -0,0 +1,33 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html lang="en-US" xmlns="http://www.w3.org/1999/xhtml" dir="ltr">
+<head>
+ <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+ <title>Сайт в разработке!</title>
+  <style type="text/css">
+   BODY {
+    background: #F7F9FB; /* цвет фона */
+    color: #fc0; /* цвет текста */
+   }
+  </style>
+</head>
+<body bgcolor="#000" style="background:url(/tpl/assets/img/abstractgradient.png)">
+
+<div>
+ <table align="center">
+   <tr>
+</br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br>
+<td><img src="/tpl/assets/img/yops.svg" /></td>   
+   
+   </tr>
+
+
+
+  </table>
+</div>
+
+
+
+
+
+</body>
+</html>
