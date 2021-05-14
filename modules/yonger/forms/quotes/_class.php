da904fee1bf90a7c4228b339e995ce630c83181f
Oleg Frolov <oleg_frolov@mail.ru>
2021-05-14 18:27:37 +0300

    update

--- modules/yonger/forms/quotes/_class.php
+++ modules/yonger/forms/quotes/_class.php
@@ -4,6 +4,7 @@
 class quotesClass extends cmsFormsClass {
     function beforeItemSave(&$item) {
         $item['login'] = $this->app->vars('_sess.user.login');
+        isset($item['status']) ? null : $item['status'] = 'new';
     }
 }
 ?>
