76f52b316d7c2c932ff962ba0dbf48f3ee8d1366
Oleg Frolov <oleg_frolov@mail.ru>
2021-09-09 23:28:22 +0300

    update

--- modules/yonger/tpl/aside_menu.inc.php
+++ modules/yonger/tpl/aside_menu.inc.php
@@ -1,69 +1,35 @@
 <div class="divider-text" wb-if='"{{_route.subdomain}}" > ""  AND "{{_sett.modules.yonger.standalone}}" !== "on"'>{{_route.subdomain}}</div>
 <hr wb-if='"{{_route.subdomain}}" == "" AND "{{_sett.modules.yonger.standalone}}" !== "on"'>
-<ul class="nav nav-aside" wb-if='"{{_route.subdomain}}" > "" OR "{{_sett.modules.yonger.standalone}}" == "on"'>
-    <li>
-        <div class="mg-y-20">Навигация</div>
-    </li>
 
-    <li class="nav-item with-sub">
-        <a href="#" data-ajax="{'url':'/cms/ajax/form/pages/list/','html':'.content-body'}" auto class="nav-link">
-            <svg class="mi mi-browser-internet-web-network-window-app-icon" wb-module="myicons"></svg>
-            <span>Страницы</span>
+<ul class="nav nav-aside" wb-tree="from=_sett.cmsmenu.data&branch=aside&parent=false">
+    <li wb-if="'{{_lvl}}'=='1' && '{{active}}'=='on'">
+        <div class="mg-y-20">{{data.label}}</div>
+    </li>
+    <li wb-if=" '{{_lvl}}' > '1' " class="nav-item with-sub">
+        <a href="#" data-ajax="{{data.ajax}}" auto class="nav-link">
+            <svg wb-if="'{{data.icon}}'>''" class="mi mi-{{data.icon}}" wb-module="myicons"></svg>
+            <span>{{data.label}}</span>
         </a>
-    </li>
-
-    <li class="nav-item with-sub">
-        <a href="#" data-ajax="{'url':'/cms/ajax/form/news/list/','html':'.content-body'}" class="nav-link">
-            <svg class="mi mi-news-2-paper" wb-module="myicons"></svg>
-            <span>Новости</span>
-        </a>
-    </li>
-
-    <li class="nav-item">
-        <a href="#" data-ajax="{'url':'/cms/ajax/form/users/list_users/','html':'.content-body'}" class="nav-link">
-            <svg class="mi mi-group-user.1" wb-module="myicons"></svg>
-            <span>Пользователи</span>
-        </a>
-    </li>
-    <li class="nav-item">
-        <a href="#" data-ajax="{'url':'/cms/ajax/form/quotes/list/','html':'.content-body'}" class="nav-link">
-            <svg class="mi mi-document-contract-edit-pen" wb-module="myicons"></svg>
-            <span>Заявки</span>
-        </a>
-    </li>
-    <li class="nav-item">
-        <a href="#" data-ajax="{'url':'/cms/ajax/form/comments/list/','html':'.content-body'}" class="nav-link">
-            <svg class="mi mi-users-message-support-1" wb-module="myicons"></svg>
-            <span>Коментарии</span>
-        </a>
-    </li>
-
-    <li class="nav-item">
-        <a href="#" data-ajax="{'url':'/module/chat/','html':'.content-body'}" class="nav-link">
-            <svg class="mi mi-chat-messages-bubble.14" wb-module="myicons"></svg>
-            <span>Сообщения</span>
-        </a>
-    </li>
-
-    <li>
-        <div class="mg-y-20">Система</div>
-    </li>
-
-    <li class="nav-item">
-        <a href="#" data-ajax="{'url':'/cms/settings/settings_ui','html':'.content-body'}" class="nav-link">
-            <svg class="mi mi-settings.2" wb-module="myicons"></svg>
-            <span>Настройки</span>
-        </a>
-    </li>
-    <li class="nav-item">
-            <a href="#" data-ajax="{'url':'/module/yonger/support','html':'.content-body'}" class="nav-link">
-                <svg class="mi mi-protection-06" wb-module="myicons"></svg>
-                <span>Поддержка</span>
-            </a>
     </li>
 </ul>
 
-<div wb-if='"{{_route.subdomain}}" == "" AND  OR "{{_sett.modules.yonger.standalone}}" !== "on"'>
+<ul class="nav nav-aside" wb-if='"{{_route.subdomain}}" > "" OR "{{_sett.modules.yonger.standalone}}" == "on"'>
+    <wb-foreach wb="from=_sett.modules.yonger.aside" wb-filter="active=on">
+    
+    <li wb-if="'{{link}}'==''">
+        <div class="mg-y-20">{{label}}</div>
+    </li>
+    <li wb-if=" '{{link}}' > '' " class="nav-item with-sub">
+        <a href="#" data-ajax="{{link}}" auto class="nav-link">
+            <svg wb-if="'{{icon}}'>''" class="mi mi-{{icon}}" wb-module="myicons"></svg>
+            <span>{{label}}</span>
+        </a>
+    </li>
+
+    </wb-foreach>
+</ul>
+
+<div wb-if='( "{{_route.subdomain}}" == "" AND "{{_sett.modules.yonger.standalone}}" !== "on" )'>
     <ul class="nav nav-aside">
         <li>
             <div class="mg-y-20">Система</div>
